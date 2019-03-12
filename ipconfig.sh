2ed289cd3670bb9ad68627527dc4be3bddf5478f
ZT588 <48389898+ZT588@users.noreply.github.com>
2019-03-12 21:27:00 +0800

    Update ipconfig.sh

--- ipconfig.sh
+++ ipconfig.sh
@@ -2,34 +2,36 @@
 
 echo "**此脚本使用于centos配置多个IP，需要手动输入网络位，掩码，网关，网卡信息**"
 
-echo -n "网络位:(192.168.1)"
+echo -n "网络位(192.168.1):"
 read num
 
 
-echo -n "起始IP:(1)"
+echo -n "起始IP(2):"
 read a
 
-echo -n "结束IP:(254)"
+echo -n "结束IP(254):"
 read b
 
-echo -n "掩码:(255.255.255.0)"
+echo -n "掩码(255.255.255.0):"
 read c
 
 echo -n "网关(最后一位):"
 read g
 
-echo -n "网卡名:"
+echo -n "网卡名(eth0):"
 read d
-for((i=a;i<=b;i++))
+echo -n "IP排序(如果是多ip段，此数字不能重复使用):"
+read h
+for((i=a,j=h;i<=b;i++,j++))
 
 
 
 do
 
   echo "
-IPADDR$i=$num.$i
-NETMASK$i=$c
-GATEWAY$i=$num.$g" >>/etc/sysconfig/network-scripts/ifcfg-$d
+IPADDR$j=$num.$i
+NETMASK$j=$c
+GATEWAY$j=$num.$g" >>/etc/sysconfig/network-scripts/ifcfg-$d
 
 
 done
