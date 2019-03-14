e384b5427f876198bfc4d570aabe2cb392e03eae
ZT588 <48389898+ZT588@users.noreply.github.com>
2019-03-15 05:44:08 +0800

    Update qa.sh

--- qa.sh
+++ qa.sh
@@ -6,8 +6,9 @@ physical=$(cat /proc/cpuinfo | grep "physical id" | sort -u | wc -l)
 processor=$(cat /proc/cpuinfo | grep "processor" | wc -l)
 cpucores=$(cat /proc/cpuinfo  | grep "cpu cores" | uniq)
 siblings=$(cat /proc/cpuinfo  | grep "siblings"  | uniq)
+freq=$( awk -F: '/cpu MHz/ {freq=$2} END {print freq}' /proc/cpuinfo | sed 's/^[ \t]*//;s/[ \t]*$//' )
 memory=$(grep MemTotal /proc/meminfo)
-
+IP=$(curl -s myip.ipip.net | awk -F ' ' '{print $2}' | awk -F '：' '{print $2}')
 disk=$(df -h)
 
 yum install mtr -y
@@ -19,19 +20,23 @@ mtr=$(mtr -c 5 101.226.197.179 --report)
 echo "================================= NOC QA REPORT================================"
 
 
-echo "系统信息 OS version       :           $osversion"
+echo "系统信息  OS version      :           $osversion"
 
-echo "系统内核 OS cores         :           $oscores"
+echo "系统内核  OS cores        :           $oscores"
 
-echo "CPU型号 cpu name          :           $cpuname"
+echo "CPU型号   cpu name        :           $cpuname"
 
-echo "物理CPU数physical id      :           $physical"
+echo "物理CPU数 physical id     :           $physical"
 
-echo "逻辑CPU数processor        :           $processor"
+echo "逻辑CPU数 processor       :           $processor"
 
-echo "CPU内核数cpu cores        :           $cpucores"
+echo "CPU       frequency       :           $freq MHZ"
+
+echo "CPU内核数 cpu cores       :           $cpucores"
 
 echo "内存大小  Memory          :           $memory"
+
+echo "IP 地址  Address          :           $IP"
 
 echo "===================================分区信息======================================"
 
@@ -41,3 +46,4 @@ echo " $disk"
 echo "===================================回国路由======================================"
 
 echo "$mtr"
+echo "================================================================================"
