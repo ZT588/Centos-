790172e29152901d8ac098ffc3ba337c22a80d5e
ZT588 <48389898+ZT588@users.noreply.github.com>
2020-05-10 19:09:24 +0800

    Create 18.04.sh

---
+++ 18.04.sh
@@ -0,0 +1,24 @@
+#/bin/bash		
+echo "**此脚本使用于ubuntu配置多个IP，需要手动输入网络位，掩码，网关，网卡信息**"
+echo -n "IP地址(192.168.1/30):"
+read num
+echo -n "网关:"
+read g
+echo -n "网卡名(eth0):"
+read d
+do
+  echo "
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    
+    $d:
+      dhcp4: no
+      dhcp6: no
+      addresses: [$num]
+      gateway4: $g
+      nameservers:
+        addresses: [8.8.8.8,8.8.4.4]" >>/etc/netplan# vi 01-netcfg.yaml
+done
+}
